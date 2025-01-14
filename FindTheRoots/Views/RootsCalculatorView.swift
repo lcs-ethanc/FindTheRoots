a3424595030aee1e807343668f5918addd27a86d
Ethan Chen <echen@lcs.on.ca>
2025-01-14 11:32:50 -0500

    made v stack with slider and text, created variable a

--- FindTheRoots/Views/RootsCalculatorView.swift
+++ FindTheRoots/Views/RootsCalculatorView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct RootsCalculatorView: View {
+    @State var a: Double = 0
     var body: some View {
         VStack {
             HStack {
@@ -22,6 +23,10 @@ struct RootsCalculatorView: View {
             Image("QuadraticEquation")
                 .resizable()
                 .frame(width: 250, height: 80)
+            VStack{
+                Slider(value: $a, in: 1...100)
+                Text("a = \(a)")
+            }
             
             Spacer()
         }
