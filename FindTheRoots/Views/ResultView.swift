d4731b79633547d3b399de80d255d26d47543678
Ethan Chen <echen@lcs.on.ca>
2025-01-14 12:21:52 -0500

    created helper view

---
+++ FindTheRoots/Views/ResultView.swift
@@ -0,0 +1,19 @@
+//
+//  ResultView.swift
+//  FindTheRoots
+//
+//  Created by Pak Ching Ethan Chen on 2025-01-14.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    let somePriorResult: Result
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView(somePriorResult: resultForPreviews)
+}
