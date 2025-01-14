35905335d96bc9b6275e13d523fb3c8781366a59
Ethan Chen <echen@lcs.on.ca>
2025-01-14 11:11:14 -0500

    Initial Commit

---
+++ FindTheRoots/FindTheRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRootsApp.swift
+//  FindTheRoots
+//
+//  Created by Pak Ching Ethan Chen on 2025-01-14.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
